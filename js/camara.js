17e9785f2d6ba8e86c5d79a2914512c512342acc
Glen112 <escobar.glen@gmail.com>
2024-01-04 11:55:36 -0300

    cadastro con foto

--- js/camara.js
+++ js/camara.js
@@ -1,6 +1,12 @@
 const btnStartCamara = document.querySelector('[data-video-botao]');
 const campoCamara = document.querySelector('[data-camera]');
 const video = document.querySelector('[data-video]');
+const opturador = document.querySelector('[data-tirar-foto]');
+const canvas = document.querySelector('[data-video-canvas]');
+const mensaje = document.querySelector('[data-mensagem]');
+const btnSendPic = document.querySelector('[data-enviar]');
+
+let imagemURL = '';
 
 btnStartCamara.addEventListener('click', async function(){
 
@@ -10,3 +16,21 @@ btnStartCamara.addEventListener('click', async function(){
 
     video.srcObject = startVideo;
 } )
+
+opturador.addEventListener('click', function(){
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    imagemURL = canvas.toDataURL('imagem/jpeg');
+    campoCamara.style.display = 'none';
+    mensaje.style.display = 'block';
+})
+
+btnSendPic.addEventListener('click', () => {
+    const recibirDatosExistentes =  localStorage.getItem('cadastro');
+    const respuestaConvertida = JSON.parse(recibirDatosExistentes)
+
+    respuestaConvertida.imagem = imagemURL;
+    localStorage.setItem('cadastro', JSON.stringify(respuestaConvertida))
+    console.log(respuestaConvertida)
+
+    window.location.href = './abrir-conta-form-3.html'
+})
